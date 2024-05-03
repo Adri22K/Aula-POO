0194f62a167a342ef7afd331d55fbc080a5dc3a9
Adri22K <168793109+Adri22K@users.noreply.github.com>
2024-05-02 23:04:07 -0300

    Add files via upload

---
+++ src/main/java/com/biblioteca/model/entity/AbstractEntity.java
@@ -0,0 +1,16 @@
+package  com.biblioteca.model.entity;
+
+public abstract class AbstractEntity {
+  private int id;
+  private int idCliente;
+
+  public int getId() {
+    return id;
+  }
+
+  public int getIdCliente() {
+    return idCliente;
+
+  }
+
+}
