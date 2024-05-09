80072540a78fa50b02e6989b2a4c2b3994773f1a
Adri22K <168793109+Adri22K@users.noreply.github.com>
2024-05-08 21:07:45 -0300

    Update DBConfig.java

--- src/main/java/com/biblioteca/config/DBConfig.java
+++ src/main/java/com/biblioteca/config/DBConfig.java
@@ -3,20 +3,32 @@ package  com.biblioteca.config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConfig {
 
-  private static final String URL = "ulr_do_banco";
+  private static final String URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
 
-  private static final String USER = "user";
+  private static final String USER = "sa";
 
-  private static final String PASSWORD = "pass";
+  private static final String PASSWORD = "";
 
   public static Connection getConnection() throws SQLException {
     return DriverManager.getConnection(URL, USER, PASSWORD);
   }
 
   public static void createTables() {
-    // TODO: Query para criar as tabelas no banco e executar ao iniciar o sistema
+    try (Connection conn = getConnection(); Statement stmt = conn.createStatement()) {
+      String sql = """
+          CREATE TABLE IF NOT EXISTS Livro (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            titulo VARCHAR(255) NOT NULL,
+            autor VARCHAR(255) NOT NULL,
+            num_paginas INT NOT NULL
+          )""";
+      stmt.executeUpdate(sql);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 }
